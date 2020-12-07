3f2e3c03a884d428ec6462d82c38f3a853ae6163
padminishreshta <54432491+padminishreshta@users.noreply.github.com>
2020-12-07 09:58:40 +0530

    Update main.py

--- gui/main.py
+++ gui/main.py
@@ -240,7 +240,7 @@ def main():
 	priyasocial.grid(row=5, sticky='SW',padx = 8)
 	text3 = Label(Aboutus, text="|")
 	text3.grid(row=5)
-	parthsocial = Label(Aboutus, text=" Gop")
+	parthsocial = Label(Aboutus, text=" Gp")
 	parthsocial.bind("<Button-1>",openparthlinkedin)
 	parthsocial.grid(row=5, sticky='SE',padx = 8)
     
